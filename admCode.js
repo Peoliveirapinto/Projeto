a32a9606da4f0167ea1e34d0b2870db972b1eb3b
Peoliveirapinto <peoliveirapinto@gmail.com>
2024-12-05 00:14:04 -0300

    identação

--- admCode.js
+++ admCode.js
@@ -1,43 +1,43 @@
 const protocolo = 'http://'
 const baseURL = 'localhost:3000'
 
-async function addAdmin(){
+async function addAdmin() {
     let adminUsernameInput = document.querySelector('#adminUsername')
     let adminUsername = adminUsernameInput.value
-        try{
-            const adminEndpoint = '/addAdmin'
-            const URLcompleta = `${protocolo}${baseURL}${adminEndpoint}`
-            await axios.post(
-                URLcompleta,
-                {login: adminUsername}
-            )
-            adminUsernameInput.value = ''
-        }
-        catch(e){
-            console.log(e)
-        }
+    try {
+        const adminEndpoint = '/addAdmin'
+        const URLcompleta = `${protocolo}${baseURL}${adminEndpoint}`
+        await axios.post(
+            URLcompleta,
+            { login: adminUsername }
+        )
+        adminUsernameInput.value = ''
+    }
+    catch (e) {
+        console.log(e)
+    }
 }
 
-async function postExercicio(){
+async function postExercicio() {
     let conteudoExercicioInput = document.querySelector('#conteudo')
     let respostaExercicioInput = document.querySelector('#resposta')
     let dataExercicioInput = document.querySelector('#data')
     let conteudoExercicio = conteudoExercicioInput.value
     let respostaExercicio = respostaExercicioInput.value
     let dataExercicio = dataExercicioInput.value
-    if (conteudoExercicio && respostaExercicio && dataExercicio){
-        try{
+    if (conteudoExercicio && respostaExercicio && dataExercicio) {
+        try {
             const exercicioEndpoint = '/postExercicio'
             const URLcompleta = `${protocolo}${baseURL}${exercicioEndpoint}`
             await axios.post(
                 URLcompleta,
-                {conteudoExercicio: conteudoExercicio, respostaExercicio: respostaExercicio, dataExercicio: dataExercicio}
+                { conteudoExercicio: conteudoExercicio, respostaExercicio: respostaExercicio, dataExercicio: dataExercicio }
             )
             conteudoExercicioInput.value = ''
             respostaExercicioInput.value = ''
             dataExercicioInput.value = ''
         }
-        catch(e){
+        catch (e) {
             console.log(e)
         }
     }
--- backend.js
+++ backend.js
@@ -1,27 +1,27 @@
-const express =require('express')
+const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 const bcrypt = require('bcrypt')
-const jwt =require('jsonwebtoken')
-const app =express()
-const { config }= require ('./config.js')
+const jwt = require('jsonwebtoken')
+const app = express()
+const { config } = require('./config.js')
 
 app.use(express.json())
 app.use(cors())
 
 //conexão de login ao banco
 const usuarioSchema = mongoose.Schema({
-    login: {type: String, required: true},
-    password: {type:String, required:true},
-    escolaridade: {type:String, required:true},
-    isAdmin: {type:Boolean, required:true}
+    login: { type: String, required: true },
+    password: { type: String, required: true },
+    escolaridade: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true }
 })
 usuarioSchema.plugin(uniqueValidator)
 const Usuario = mongoose.model('Usuario', usuarioSchema)
 
-app.post("/signup", async(req,res)=>{
-    try{
+app.post("/signup", async (req, res) => {
+    try {
         const login = req.body.login
         const password = req.body.password
         const escolaridade = req.body.escolaridade
@@ -36,7 +36,7 @@ app.post("/signup", async(req,res)=>{
         const respMongo = await usuario.save()
         console.log(respMongo)
         res.status(201).end()
-    } catch (error){
+    } catch (error) {
         console.log(error)
         res.status(409).end()
     }
@@ -53,22 +53,22 @@ app.post("/login", async (req, res) => {
     if (!senhaValida) {
         return res.status(401).json({ mensagem: "senha inválida" })
     }
-    const token =jwt.sign(
-        {login: login},
+    const token = jwt.sign(
+        { login: login },
         "chave-secreta",
-        {expiresIn: '1h'}
+        { expiresIn: '1h' }
     )
-    res.status(200).json({token: token})
+    res.status(200).json({ token: token })
 })
 
-app.post("/addAdmin", async (req,res)=>{
-    try{
-        const isAdmin = {isAdmin:true}
-        const novoAdmin = await Usuario.findOneAndUpdate({login:req.body.login}, {isAdmin:isAdmin})
+app.post("/addAdmin", async (req, res) => {
+    try {
+        const isAdmin = { isAdmin: true }
+        const novoAdmin = await Usuario.findOneAndUpdate({ login: req.body.login }, { isAdmin: isAdmin })
         const respMongo = await novoAdmin.save()
         console.log(respMongo)
         res.status(201).end()
-    } catch(error){
+    } catch (error) {
         console.log(error)
         res.status(409).end()
     }
@@ -76,50 +76,50 @@ app.post("/addAdmin", async (req,res)=>{
 
 //conexão das dúvidas do fórum ao banco
 const duvidaSchema = mongoose.Schema({
-    tituloDuvida: {type:String, required:true},
-    conteudoDuvida: {type: String, required:true},
-    user: {type:String, required:true},
-    isResolvida: {type:Boolean, required:true},
+    tituloDuvida: { type: String, required: true },
+    conteudoDuvida: { type: String, required: true },
+    user: { type: String, required: true },
+    isResolvida: { type: Boolean, required: true },
 })
 duvidaSchema.plugin(uniqueValidator)
 const Duvida = mongoose.model('Duvida', duvidaSchema)
 
-app.post("/postDuvida", async (req,res)=>{
-    try{
+app.post("/postDuvida", async (req, res) => {
+    try {
         const tituloDuvida = req.body.tituloDuvida
-        const conteudoDuvida =req.body.conteudoDuvida
-        const user =req.body.user
+        const conteudoDuvida = req.body.conteudoDuvida
+        const user = req.body.user
         const isResolvida = false
         const duvida = new Duvida({
             tituloDuvida: tituloDuvida,
-            conteudoDuvida:conteudoDuvida,
+            conteudoDuvida: conteudoDuvida,
             user: user,
             isResolvida: isResolvida
         })
         const respMongo = await duvida.save()
         console.log(respMongo)
         res.status(201).end()
-    } catch(error){
+    } catch (error) {
         console.log(error)
         res.status(409).end()
     }
 })
 
-app.get("/getDuvidas", async (req,res)=>{
+app.get("/getDuvidas", async (req, res) => {
     const duvidas = await Duvida.find()
     res.json(duvidas)
 })
 
 //conexão das respostas das dúvidas do fórum ao banco
 const respDuvidaSchema = mongoose.Schema({
-    conteudoRespDuvida: {type: String, required:true},
-    idDuvida: {type: Number, required: true}
+    conteudoRespDuvida: { type: String, required: true },
+    idDuvida: { type: Number, required: true }
 })
 respDuvidaSchema.plugin(uniqueValidator)
-const RespDuvida =mongoose.model('RespDuvida',respDuvidaSchema)
+const RespDuvida = mongoose.model('RespDuvida', respDuvidaSchema)
 
-app.post("/postRespDuvida", async (req,res)=>{
-    try{
+app.post("/postRespDuvida", async (req, res) => {
+    try {
         const conteudoRespDuvida = req.body.conteudoRespDuvida
         const idDuvida = req.body.idDuvida
         const respDuvida = new RespDuvida({
@@ -129,28 +129,28 @@ app.post("/postRespDuvida", async (req,res)=>{
         const respMongo = await respDuvida.save()
         console.log(respMongo)
         res.status(201).end()
-    } catch(error){
+    } catch (error) {
         console.log(error)
         res.status(409).end()
     }
 })
 
-app.get("/getRespDuvida", async (req,res)=>{
-    const resposta = await RespDuvida.findOne({idDuvida: req.body.idDuvida})
+app.get("/getRespDuvida", async (req, res) => {
+    const resposta = await RespDuvida.findOne({ idDuvida: req.body.idDuvida })
     res.json(resposta)
 })
 
 //conexão dos exercícios ao banco
 const exerciocioSchema = mongoose.Schema({
-    conteudoExercicio: {type:String, required:true},
-    respostaExercicio: {type:String, required:true},
-    dataExercicio: {type:Date, required:true}
+    conteudoExercicio: { type: String, required: true },
+    respostaExercicio: { type: String, required: true },
+    dataExercicio: { type: Date, required: true }
 })
 exerciocioSchema.plugin(uniqueValidator)
 const Exercicio = mongoose.model('Exercicio', exerciocioSchema)
 
-app.post("/postExercicio", async (req, res)=>{
-    try{
+app.post("/postExercicio", async (req, res) => {
+    try {
         const conteudoExercicio = req.body.conteudoExercicio
         const respostaExercicio = req.body.respostaExercicio
         const dataExercicio = req.body.dataExercicio
@@ -162,14 +162,14 @@ app.post("/postExercicio", async (req, res)=>{
         const respMongo = await exercicio.save()
         console.log(respMongo)
         res.status(201).end()
-    } catch (error){
+    } catch (error) {
         console.log(error)
         res.status(409).end()
     }
 })
 
-app.get("/getExercicio", async (req,res)=>{
-    const exercicio = await Exercicio.findOne({dataExercicio:req.body.dataExercicio})
+app.get("/getExercicio", async (req, res) => {
+    const exercicio = await Exercicio.findOne({ dataExercicio: req.body.dataExercicio })
     res.json(exercicio)
 })
 
