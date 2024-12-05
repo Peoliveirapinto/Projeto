ad8bc7c764d8ccffb59aaa1df44f3a0d8fde5c63
Peoliveirapinto <peoliveirapinto@gmail.com>
2024-12-04 22:21:42 -0300

    login e cadastro tela forum

---
+++ loginCode.js
@@ -0,0 +1,65 @@
+const protocolo = 'http://'
+const baseURL = 'localhost:3000'
+
+async function prepararPagina(){
+    const token = localStorage.getItem("token")
+    const loginLink = document.querySelector('#loginLink')
+    if (token){
+        loginLink.innerHTML = 'Logout'
+    }
+    else{
+        loginLink.innerHTML = 'Login'
+    }
+}
+
+async function cadastrarUsuario() {
+    let usuarioCadastroInput = document.querySelector('#registerUsername')
+    let senhaCadastroInput = document.querySelector('#registerPassword')
+    let escolaridadeInput = document.querySelector('#educationLevel')
+    let usuarioCadastro =usuarioCadastroInput.value
+    let senhaCadastro = senhaCadastroInput.value
+    let escolaridade = escolaridadeInput.value
+    if (usuarioCadastro && senhaCadastro){
+        try{
+            const cadastroEndpoint ='/signup'
+            const URLcompleta =`${protocolo}${baseURL}${cadastroEndpoint}`
+            await axios.post(
+                URLcompleta,
+                {login: usuarioCadastro, password: senhaCadastro, escolaridade: escolaridade}
+            )
+            usuarioCadastroInput.value =''
+            senhaCadastroInput.value =''
+        }
+        catch(e){
+            senhaCadastroInput.value =''
+        }
+    }
+}
+
+const loginUsuario = async () =>{
+    let usuarioLoginInput = document.querySelector('#username')
+    let senhaLoginInput = document.querySelector('#password')
+    let usuarioLogin =usuarioLoginInput.value
+    let senhaLogin =senhaLoginInput.value
+    if (usuarioLogin && senhaLogin){
+        try {
+            const loginEndpoint = '/login'
+            const URLcompleta  = `${protocolo}${baseURL}${loginEndpoint}`
+            const response = await axios.post(
+                URLcompleta,
+                {
+                    login: usuarioLogin,
+                    password: senhaLogin
+                }
+            )
+            localStorage.setItem("token",response.data)
+            usuarioLoginInput.value = ""
+            senhaLoginInput.value=""
+            const loginLink = document.querySelector('#loginLink')
+            loginLink.innerHTML ='logout'
+        } 
+        catch (e) {
+            senhaCadastroInput.value =''
+        }
+    }
+}
